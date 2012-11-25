20818e1ce7983b73d2542987574d36d44fc153a7
Michael Lavrisha <michael.lavrisha@gmail.com>
2012-11-24 19:47:42 -0700

    remove spec dir, because really, I didn't write tests

--- spec/lib/cyclist_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-module Sinatra
-  describe Cyclist do
-    it "registers itself as an extension" do
-      Base.extensions.should include Sinatra::Cyclist
-    end
-  end
-end
--- spec/spec_helper.rb
+++
@@ -1,6 +0,0 @@
-require "rspec"
-require "sinatra"
-require "sinatra/cyclist"
-
-Sinatra::Application.environment = :test
-Bundler.require :default, Sinatra::Application.environment
